6b26717e0aaeab42ad0ab3875ae425fcd1b056d0
Aaron K <kearlyaa@gmail.com>
2016-03-23 18:07:11 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HowFit::Application.config.secret_key_base = '0f089950de0df6a41fad47fb07a4c44e44ef78dc410111c1157c5be20156ea6bc62cb30fc74954f1ec6c2b3099676894877de4f40ae0b94546b0ae70a484abc8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HowFit::Application.config.session_store :cookie_store, key: '_howFit_session'
